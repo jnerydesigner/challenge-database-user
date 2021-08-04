5025374dd1d842e2f57a95732206e979759eb965
jnerydesigner <jander.webmaster@gmail.com>
2021-08-04 14:41:52 -0400

    Error: Account not exists

---
+++ src/modules/users/useCases/authenticateUser/IncorrectEmailOrPasswordError.ts
@@ -0,0 +1,8 @@
+
+import { AppError } from "../../../../shared/errors/AppError";
+
+export class IncorrectEmailOrPasswordError extends AppError {
+  constructor() {
+    super("Incorrect email or password", 401);
+  }
+}
